f95e3fe74d273ac669027c35043551abadf3036c
Ilyes Bouchlaghem <ilyesbouchlaghem@gmail.com>
2018-03-07 16:37:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install virtualenv
-virtualenv venv --system-site-packages
+pip install --user virtualenv
+python $HOME/Library/Python/2.7/lib/python/site-packages/virtualenv.py venv --system-site-packages -p /usr/bin/python2.7 --distribute
 source venv/bin/activate
 pip install -r requirements.txt
